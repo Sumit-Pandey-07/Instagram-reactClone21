7f098f02074ffcc29084d21f8b8d2b8546cd84b0
Sumit Pandey <73009052+Sumit-Pandey-07@users.noreply.github.com>
2022-07-16 09:04:16 +0530

    Add files via upload

---
+++ build/precache-manifest.c1dbc6c8341f3f7caae04eb8512073fc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6679c57f421444e7241b9cad93a3bfe2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4516b4bc0d62b9453b79",
+    "url": "/static/css/2.c60feeff.chunk.css"
+  },
+  {
+    "revision": "4516b4bc0d62b9453b79",
+    "url": "/static/js/2.775e6977.chunk.js"
+  },
+  {
+    "revision": "cfc5a9c2d291b699250fd9feb4a92adf",
+    "url": "/static/js/2.775e6977.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d544d0553f99020883b7",
+    "url": "/static/js/3.7a952009.chunk.js"
+  },
+  {
+    "revision": "386348aaeb0f9a5988e3",
+    "url": "/static/js/main.dcd3a967.chunk.js"
+  },
+  {
+    "revision": "bee28df64c197c065097",
+    "url": "/static/js/runtime-main.59c55f7d.js"
+  },
+  {
+    "revision": "cbb9b56a5cf892318fc8ab502f9d3ba7",
+    "url": "/static/media/facebook-icon-blue.cbb9b56a.svg"
+  },
+  {
+    "revision": "a9a13771e5dcd73931e0b61df2faac62",
+    "url": "/static/media/icon-sheet-2.a9a13771.png"
+  },
+  {
+    "revision": "3dfb71b4bb35ca8b35728b9475b7d861",
+    "url": "/static/media/icon-sheet.3dfb71b4.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
